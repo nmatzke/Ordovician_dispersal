909c5a566f1234c99e439742198b0e2dda4efeeb
nmatzke <matzke@berkeley.edu>
2017-03-29 12:38:02 +1100

    starter upload
    BEASTmasteR files are at: https://github.com/nmatzke/BEASTmasteR

---
+++ 08_SuppMat_testing_fossils_Lagrange_vs_BGB/fossils_in_Lagrange/Psychotria_M3_mod_BGB/script_v1.R
@@ -0,0 +1,353 @@
+# Load the package (after installation, see above).
+library(optimx)         # You need to have some version of optimx available
+                        # as it is a BioGeoBEARS dependency; however, if you
+                        # don't want to use optimx, and use optim() (from R core) 
+                        # you can set:
+                        # BioGeoBEARS_run_object$use_optimx = FALSE
+                        # ...everything should work either way -- NJM 2014-01-08
+library(FD)       # for FD::maxent() (make sure this is up-to-date)
+library(snow)     # (if you want to use multicore functionality; some systems/R versions prefer library(parallel), try either)
+library(parallel)
+library(BioGeoBEARS)
+
+########################################################
+# TO GET THE OPTIMX/OPTIM FIX, AND THE UPPASS FIX, 
+# SOURCE THE REVISED FUNCTIONS WITH THESE COMMANDS
+#
+# CRUCIAL CRUCIAL CRUCIAL: 
+# YOU HAVE TO RUN THE SOURCE COMMANDS AFTER 
+# *EVERY TIME* YOU DO library(BioGeoBEARS). THE CHANGES ARE NOT "PERMANENT", 
+# THEY HAVE TO BE MADE EACH TIME.  IF YOU ARE GOING TO BE OFFLINE, 
+# YOU CAN DOWNLOAD EACH .R FILE TO YOUR HARD DRIVE AND REFER THE source()
+# COMMANDS TO THE FULL PATH AND FILENAME OF EACH FILE ON YOUR
+# LOCAL SYSTEM INSTEAD.
+########################################################
+library(BioGeoBEARS)
+source("http://phylo.wdfiles.com/local--files/biogeobears/cladoRcpp.R") # (needed now that traits model added; source FIRST!)
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_add_fossils_randomly_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_basics_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_calc_transition_matrices_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_classes_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_detection_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_DNA_cladogenesis_sim_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_extract_Qmat_COOmat_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_generics_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_models_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_on_multiple_trees_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_plots_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_readwrite_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_simulate_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_SSEsim_makePlots_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_SSEsim_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_stochastic_mapping_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_stratified_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_univ_model_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/calc_uppass_probs_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/calc_loglike_sp_v01.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/get_stratified_subbranch_top_downpass_likelihoods_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/runBSM_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/stochastic_map_given_inputs.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/summarize_BSM_tables_v1.R")
+source("http://phylo.wdfiles.com/local--files/biogeobears/BioGeoBEARS_traits_v1.R") # added traits model
+calc_loglike_sp = compiler::cmpfun(calc_loglike_sp_prebyte)    # crucial to fix bug in uppass calculations
+calc_independent_likelihoods_on_each_branch = compiler::cmpfun(calc_independent_likelihoods_on_each_branch_prebyte)
+    # slight speedup hopefully
+
+#######################################################
+# Local source()-ing method -- uses BioGeoBEARS sourceall() function 
+# on a directory of .R files, so you don't have to type them out.
+# The directories here are on my machine, you would have to make a 
+# directory, save the .R files there, and refer to them.
+#
+# NOTE: it's best to source the "cladoRcpp.R" update first, to avoid warnings like this:
+##
+## Note: possible error in 'rcpp_calc_anclikes_sp_COOweights_faster(Rcpp_leftprobs = tmpca_1, ': 
+##         unused arguments (m = m, m_null_range = include_null_range, jts_matrix = jts_matrix) 
+##
+#
+# TO USE: Delete or comment out the 'source("http://...")' commands above, and un-comment
+#              the below...
+########################################################################
+# Un-comment (and fix directory paths) to use:
+#library(BioGeoBEARS)
+#source("/drives/Dropbox/_njm/__packages/cladoRcpp_setup/cladoRcpp.R")
+#sourceall("/drives/Dropbox/_njm/__packages/BioGeoBEARS_setup/")
+#calc_loglike_sp = compiler::cmpfun(calc_loglike_sp_prebyte)    # crucial to fix bug in uppass calculations
+#calc_independent_likelihoods_on_each_branch = compiler::cmpfun(calc_independent_likelihoods_on_each_branch_prebyte)
+########################################################################
+
+#######################################################
+# SETUP: YOUR WORKING DIRECTORY
+#######################################################
+# You will need to set your working directory to match your local system
+
+# Note these very handy functions!
+# Command "setwd(x)" sets your working directory
+# Command "getwd()" gets your working directory and tells you what it is.
+# Command "list.files()" lists the files in your working directory
+# To get help on any command, use "?".  E.g., "?list.files"
+
+# Set your working directory for output files
+# default here is your home directory ("~")
+# Change this as you like
+wd = "/drives/GDrive/__GDrive_projects/2016-02-04_Reddy_Condamine/DECX_biorXiv/Appendix_7_Fossil_tips_Palpimanoids/fossils_in_Lagrange/Psychotria_M3_mod_BGB/"
+setwd(wd)
+
+# Double-check your working directory with getwd()
+getwd()
+
+#######################################################
+# SETUP: Extension data directory
+#######################################################
+# When R packages contain extra files, they are stored in the "extdata" directory 
+# inside the installed package.
+#
+# BioGeoBEARS contains various example files and scripts in its extdata directory.
+# 
+# Each computer operating system might install BioGeoBEARS in a different place, 
+# depending on your OS and settings. 
+# 
+# However, you can find the extdata directory like this:
+extdata_dir = np(system.file("extdata", package="BioGeoBEARS"))
+extdata_dir
+list.files(extdata_dir)
+
+# "system.file" looks in the directory of a specified package (in this case BioGeoBEARS)
+# The function "np" is just a shortcut for normalizePath(), which converts the 
+# path to the format appropriate for your system (e.g., Mac/Linux use "/", but 
+# Windows uses "\\", if memory serves).
+
+# Even when using your own data files, you should KEEP these commands in your 
+# script, since the plot_BioGeoBEARS_results function needs a script from the 
+# extdata directory to calculate the positions of "corners" on the plot. This cannot
+# be made into a straight up BioGeoBEARS function because it uses C routines 
+# from the package APE which do not pass R CMD check for some reason.
+
+#######################################################
+# SETUP: YOUR TREE FILE AND GEOGRAPHY FILE
+#######################################################
+# Example files are given below. To run your own data,
+# make the below lines point to your own files, e.g.
+# trfn = "/mydata/frogs/frogBGB/tree.newick"
+# geogfn = "/mydata/frogs/frogBGB/geog.data"
+
+#######################################################
+# Phylogeny file
+# Notes: 
+# 1. Must be binary/bifurcating: no polytomies
+# 2. No negative branchlengths (e.g. BEAST MCC consensus trees sometimes have negative branchlengths)
+# 3. Be careful of very short branches, as BioGeoBEARS will interpret ultrashort branches as direct ancestors
+# 4. You can use non-ultrametric trees, but BioGeoBEARS will interpret any tips significantly below the 
+#    top of the tree as fossils!  This is only a good idea if you actually do have fossils in your tree,
+#    as in e.g. Wood, Matzke et al. (2013), Systematic Biology.
+# 5. The default settings of BioGeoBEARS make sense for trees where the branchlengths are in units of 
+#    millions of years, and the tree is 1-1000 units tall. If you have a tree with a total height of
+#    e.g. 0.00001, you will need to adjust e.g. the max values of d and e, or (simpler) multiply all
+#    your branchlengths to get them into reasonable units.
+# 6. DON'T USE SPACES IN SPECIES NAMES, USE E.G. "_"
+#######################################################
+# This is the example Newick file for Hawaiian Psychotria
+# (from Ree & Smith 2008)
+# "trfn" = "tree file name"
+trfn = "Psychotria_5.2.newick"
+
+# Look at the raw Newick file:
+moref(trfn)
+
+# Look at your phylogeny:
+tr = read.tree(trfn)
+tr
+plot(tr)
+title("Example Psychotria phylogeny from Ree & Smith (2008)")
+axisPhylo() # plots timescale
+
+#######################################################
+# Geography file
+# Notes:
+# 1. This is a PHLYIP-formatted file. This means that in the 
+#    first line, 
+#    - the 1st number equals the number of rows (species)
+#    - the 2nd number equals the number of columns (number of areas)
+# 2. This is the same format used for C++ LAGRANGE geography files.
+# 3. All names in the geography file must match names in the phylogeny file.
+# 4. DON'T USE SPACES IN SPECIES NAMES, USE E.G. "_"
+# 5. Operational taxonomic units (OTUs) should ideally be phylogenetic lineages, 
+#    i.e. genetically isolated populations.  These may or may not be identical 
+#    with species.  You would NOT want to just use specimens, as each specimen 
+#    automatically can only live in 1 area, which will typically favor DEC+J 
+#    models.  This is fine if the species/lineages really do live in single areas,
+#    but you wouldn't want to assume this without thinking about it at least. 
+#    In summary, you should collapse multiple specimens into species/lineages if 
+#    data indicates they are the same genetic population.
+######################################################
+
+# This is the example geography file for Hawaiian Psychotria
+# (from Ree & Smith 2008)
+geogfn = "Psychotria_geog.data"
+
+# Look at the raw geography text file:
+moref(geogfn)
+
+# Look at your geographic range data:
+tipranges = getranges_from_LagrangePHYLIP(lgdata_fn=geogfn)
+tipranges
+
+# Set the maximum number of areas any species may occupy; this cannot be larger 
+# than the number of areas you set up, but it can be smaller.
+max_range_size = 4
+
+####################################################
+####################################################
+# KEY HINT: The number of states (= number of different possible geographic ranges)
+# depends on (a) the number of areas and (b) max_range_size.
+# If you have more than about 500-600 states, the calculations will get REALLY slow,
+# since the program has to exponentiate a matrix of e.g. 600x600.  Often the computer
+# will just sit there and crunch, and never get through the calculation of the first
+# likelihood.
+# 
+# (this is also what is usually happening when LAGRANGE hangs: you have too many states!)
+#
+# To check the number of states for a given number of ranges, try:
+numstates_from_numareas(numareas=4, maxareas=4, include_null_range=TRUE)
+numstates_from_numareas(numareas=4, maxareas=4, include_null_range=FALSE)
+numstates_from_numareas(numareas=4, maxareas=3, include_null_range=TRUE)
+numstates_from_numareas(numareas=4, maxareas=2, include_null_range=TRUE)
+
+# Large numbers of areas have problems:
+numstates_from_numareas(numareas=10, maxareas=10, include_null_range=TRUE)
+
+# ...unless you limit the max_range_size:
+numstates_from_numareas(numareas=10, maxareas=2, include_null_range=TRUE)
+####################################################
+####################################################
+
+#######################################################
+#######################################################
+# DEC AND DEC+J ANALYSIS
+#######################################################
+#######################################################
+# NOTE: The BioGeoBEARS "DEC" model is identical with 
+# the Lagrange DEC model, and should return identical
+# ML estimates of parameters, and the same 
+# log-likelihoods, for the same datasets.
+#
+# Ancestral state probabilities at nodes will be slightly 
+# different, since BioGeoBEARS is reporting the 
+# ancestral state probabilities under the global ML
+# model, and Lagrange is reporting ancestral state
+# probabilities after re-optimizing the likelihood
+# after fixing the state at each node. These will 
+# be similar, but not identical. See Matzke (2014),
+# Systematic Biology, for discussion.
+#
+# Also see Matzke (2014) for presentation of the 
+# DEC+J model.
+#######################################################
+#######################################################
+
+#######################################################
+#######################################################
+
+#######################################################
+# Run DEC
+#######################################################
+
+# Intitialize a default model (DEC model)
+BioGeoBEARS_run_object = define_BioGeoBEARS_run()
+
+# Give BioGeoBEARS the location of the phylogeny Newick file
+BioGeoBEARS_run_object$trfn = trfn
+
+# Give BioGeoBEARS the location of the geography text file
+BioGeoBEARS_run_object$geogfn = geogfn
+
+# Input the maximum range size
+BioGeoBEARS_run_object$max_range_size = max_range_size
+
+BioGeoBEARS_run_object$min_branchlength = 0.000001    # Min to treat tip as a direct ancestor (no speciation event)
+BioGeoBEARS_run_object$include_null_range = TRUE    # set to FALSE for e.g. DEC* model, DEC*+J, etc. (see Massana et al.)
+# also search script on "include_null_range" for other places to change
+
+# Set up a time-stratified analysis:
+# 1. Here, un-comment ONLY the files you want to use.
+# 2. Also un-comment "BioGeoBEARS_run_object = section_the_tree(...", below.
+# 3. For example files see (a) extdata_dir, 
+#  or (b) http://phylo.wikidot.com/biogeobears#files
+#  and BioGeoBEARS Google Group posts for further hints)
+#
+# Uncomment files you wish to use in time-stratified analyses:
+BioGeoBEARS_run_object$timesfn = "timeperiods.txt"
+BioGeoBEARS_run_object$dispersal_multipliers_fn = "dispersal_multipliers.txt"
+#BioGeoBEARS_run_object$areas_allowed_fn = "areas_allowed.txt"
+#BioGeoBEARS_run_object$areas_adjacency_fn = "areas_adjacency.txt"
+#BioGeoBEARS_run_object$distsfn = "distances_matrix.txt"
+# See notes on the distances model on PhyloWiki's BioGeoBEARS updates page.
+
+# Speed options and multicore processing if desired
+BioGeoBEARS_run_object$speedup = TRUE          # shorcuts to speed ML search; use FALSE if worried (e.g. >3 params)
+BioGeoBEARS_run_object$use_optimx = TRUE     # if FALSE, use optim() instead of optimx()
+BioGeoBEARS_run_object$num_cores_to_use = 1
+# (use more cores to speed it up; this requires
+# library(parallel) and/or library(snow). The package "parallel" 
+# is now default on Macs in R 3.0+, but apparently still 
+# has to be typed on some Windows machines. Note: apparently 
+# parallel works on Mac command-line R, but not R.app.
+# BioGeoBEARS checks for this and resets to 1
+# core with R.app)
+
+# Sparse matrix exponentiation is an option for huge numbers of ranges/states (600+)
+# I have experimented with sparse matrix exponentiation in EXPOKIT/rexpokit,
+# but the results are imprecise and so I haven't explored it further.
+# In a Bayesian analysis, it might work OK, but the ML point estimates are
+# not identical.
+# Also, I have not implemented all functions to work with force_sparse=TRUE.
+# Volunteers are welcome to work on it!!
+BioGeoBEARS_run_object$force_sparse = FALSE    # force_sparse=TRUE causes pathology & isn't much faster at this scale
+
+# This function loads the dispersal multiplier matrix etc. from the text files into the model object. Required for these to work!
+# (It also runs some checks on these inputs for certain errors.)
+BioGeoBEARS_run_object = readfiles_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+# Divide the tree up by timeperiods/strata (uncomment this for stratified analysis)
+BioGeoBEARS_run_object = section_the_tree(inputs=BioGeoBEARS_run_object, make_master_table=TRUE, plot_pieces=FALSE, cut_fossils=FALSE)
+# The stratified tree is described in this table:
+#BioGeoBEARS_run_object$master_table
+
+# Good default settings to get ancestral states
+BioGeoBEARS_run_object$return_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_TTL_loglike_from_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_ancprobs = TRUE    # get ancestral states from optim run
+
+# Set up DEC model
+# (nothing to do; defaults)
+
+# Look at the BioGeoBEARS_run_object; it's just a list of settings etc.
+BioGeoBEARS_run_object
+
+# This contains the model object
+BioGeoBEARS_run_object$BioGeoBEARS_model_object
+
+# This table contains the parameters of the model 
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table
+
+# Run this to check inputs. Read the error messages if you get them!
+check_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+# For a slow analysis, run once, then set runslow=FALSE to just 
+# load the saved result.
+runslow = TRUE
+resfn = "Psychotria_DEC_M3_stratified_v1.Rdata"
+if (runslow)
+    {
+    res = bears_optim_run(BioGeoBEARS_run_object)
+    res    
+
+    save(res, file=resfn)
+    resDEC = res
+    } else {
+    # Loads to "res"
+    load(resfn)
+    resDEC = res
+    }
+
+
